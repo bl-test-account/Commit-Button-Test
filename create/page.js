31259b8436ff45017e127829f45174a00241b4b5
bl-test-account <plugin.developer@bytelight.com>
2013-02-01 21:21:11 -0800

    Saved at Sat Feb 02 2013 00:20:51 GMT-0500 (EST)

--- create/page.js
+++ create/page.js
@@ -1,4 +1,4 @@
 $(function() {
-  $('p').text('Hello World: this is the create page for adding a content item ' +
+  $('p').text('Hello Worlds: this is the create page for adding a content item ' +
               'to the timeline');
 })
