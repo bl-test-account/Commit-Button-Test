f1f8d2d10bb6a4ccf6e88d0c343c94cec96bd7d4
bl-test-account <plugin.developer@bytelight.com>
2013-02-04 11:19:15 -0800

    Pink

--- create/page.js
+++ create/page.js
@@ -1,4 +1,4 @@
 $(function() {
-  $('p').text('Hi Worlds: this is the create page for adding a content item ' +
+  $('p').text('Hello Worlds: this is the create page for adding a content item ' +
               'to the timeline');
 })
